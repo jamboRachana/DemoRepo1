584bfc3dd9edd81849c5e5b68d9aff46a1469cf4
jamboRachana <rachanatakawale@gmail.com>
2024-04-03 19:52:08 +0530

    bug-122 fixed

--- demo.java
+++ demo.java
@@ -4,4 +4,11 @@ public static  void main(String args[])
    {
      system.out.println("hello java"); 
     }
+//bug-122 fix changes
+  void add()
+{
+   system.out.println("add java");
+}
+//logic is in progress
+
 }
